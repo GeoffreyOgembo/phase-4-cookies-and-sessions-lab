c19b3172ac00cd9a780066a55bde9a565830e044
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-10-04 10:13:26 -0400

    Done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :page_limit_reached
+
+  def increment_views
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+  end
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -8,13 +14,24 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    # render json: article
+    increment_views 
+    if session[:page_views] >= 0 && session[:page_views] <= 3
+      article = Article.find(params[:id])
+      # render json: article
+    else
+      page_limit_reached
+    end
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+    render json: { error: 'Article not found' }, status: :not_found
+  end
+  def page_limit_reached
+    render json: { error: 'Maximum pageview limit reached' }, status: 401
   end
 
 end
